25bc64201f048d77ae1364842b64526d9cf85af9
bseyi <bseyi39@gmail.com>
2022-06-21 17:53:03 -0700

    Initial commit
    SignUp and Login Feature

---
+++ app/src/main/java/com/example/artmates/LandingPageActivity.java
@@ -0,0 +1,70 @@
+package com.example.artmates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.parse.ParseUser;
+
+public class LandingPageActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private Button btnSignUp;
+    private ImageView icon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+//        if(ParseUser.getCurrentUser() != null) {
+//            goMainActivity();
+//        }
+        icon = findViewById(R.id.icon);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.anim);
+        icon.startAnimation(animation);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLogin();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSignUp();
+            }
+        });
+    }
+
+    private void onLogin() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void onSignUp() {
+        Intent i = new Intent(this, SignupActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
+
---
+++ app/src/main/java/com/example/artmates/SignupActivity.java
@@ -0,0 +1,87 @@
+package com.example.artmates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+    private EditText etNewUser;
+    private EditText etEmail;
+    private EditText etNewPassword;
+    private Button  goToSignIn;
+    private Button btnSignUp2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        etNewUser = findViewById(R.id.etNewUser);
+        etEmail = findViewById(R.id.etEmail);
+        etNewPassword = findViewById(R.id.etNewPassword);
+        goToSignIn = findViewById(R.id.goToSignIn);
+        btnSignUp2 = findViewById(R.id.btnSignUp2);
+
+        goToSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginActivity();
+            }
+        });
+
+        btnSignUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etNewUser.getText().toString();
+                String password = etNewPassword.getText().toString();
+                String email = etEmail.getText().toString();
+
+                signUpUser(username, password, email);
+                goToLoginActivity();
+            }
+        });
+
+    }
+
+    private void signUpUser(String username, String password, String email) {
+        Log.i(TAG, "Attempting to login user");
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    Toast.makeText(SignupActivity.this, "Success signing up", Toast.LENGTH_SHORT).show();
+                    goToLoginActivity();
+                }
+                else{
+                    Toast.makeText(SignupActivity.this, "Error signing up", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Error signing up");
+                }
+            }
+        });
+    }
+
+    public  void goToLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+}
+
